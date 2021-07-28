e2e98f2dbea9de1fd5021f9577fb4d04ae37cee0
tishajain <74290165+tishajain@users.noreply.github.com>
2021-07-28 17:36:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song = "";
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function setup() 
 {
@@ -8,8 +11,16 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded() 
+{
+    console.log('PoseNet is Initialised');
+
+}
 
 function draw() 
 {
@@ -26,3 +37,21 @@ function play()
     song.play();
 }
 
+
+function gotPoses(results) 
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY)
+    }
+}
